4fb1ef96f0cc1df09e652b4a8b065f95627286c7
Quliyevh23 <aliquliyev1290@gmail.com>
2024-08-25 14:24:49 +0400

    Homework done

---
+++ Homework25avqust/Homework25avqust/Program.cs
@@ -0,0 +1,40 @@
+﻿#region Task1 
+
+//int a = 10; 
+//int b = 90; 
+//int s = a;
+//while (s != b)
+//{
+//    s++;
+//    if(s % 3 == 0)
+//    {
+//        Console.WriteLine(s);
+//    }
+//}
+
+#endregion
+
+
+
+
+
+#region Task2
+
+
+//int num = 8;
+//while (num % 2 == 0)
+//{
+//    num /= 2;
+//}
+//if (num == 1)
+//{
+//    Console.WriteLine("quvvetidir");
+//}
+
+//else
+//{
+//    Console.WriteLine("quvveti deyil");
+//}
+
+
+#endregion
